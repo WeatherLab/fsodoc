4c098541659c734b4be7522876b0a54852058de4
Xin Zhang <xin.l.zhang@noaa.gov>
2018-12-28 07:08:44 +0800

    Updates:

--- source/conf.py
+++ source/conf.py
@@ -84,8 +84,8 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'trac'
-highlight_language = 'ci'
+pygments_style = 'sphinx'
+# highlight_language = 'ci'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
